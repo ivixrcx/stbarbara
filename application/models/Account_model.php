42ff853f4a085d3985859c8bc76595682220f78a
Mark Daryl Jerezon <markdaryljerezon@gmail.com>
2019-03-28 17:30:33 +0800

    make auth_login

---
+++ application/models/Account_model.php
@@ -0,0 +1,34 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Account_model extends CI_Model {
+
+	/**
+	 * Index Page for this controller.
+	 *
+	 * Maps to the following URL
+	 * 		http://example.com/index.php/welcome
+	 *	- or -
+	 * 		http://example.com/index.php/welcome/index
+	 *	- or -
+	 * Since this controller is set as the default controller in
+	 * config/routes.php, it's displayed at http://example.com/
+	 *
+	 * So any other public methods not prefixed with an underscore will
+	 * map to /index.php/welcome/<method_name>
+	 * @see https://codeigniter.com/user_guide/general/urls.html
+	 */
+
+	public function auth_login($user_name, $password)
+	{
+		return $this->db->select('*')
+		->from('user')
+		->where( 
+			array(
+				'user_name' => $user_name,
+				'password' => $password
+			) 
+		)
+		->count_all();
+	}
+}
