f465e872083819788bcf978370327e7f7f47425b
livil <markdaryljerezon@gmail.com>
2019-05-12 12:04:36 +0800

    config changes

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = "$_SERVER[REQUEST_SCHEME]://$_SERVER[HTTP_HOST]/stbarbara";
+$config['base_url'] = "$_SERVER[REQUEST_SCHEME]://$_SERVER[HTTP_HOST]/repositories/stbarbara/";
 
 /*
 |--------------------------------------------------------------------------
