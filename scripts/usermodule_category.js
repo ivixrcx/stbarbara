a906e60ae83f6ecdeeed7f8c063bfad6a3142008
Mark Daryl Jerezon <markdaryljerezon@gmail.com>
2019-07-30 15:31:35 +0800

    modified redirection

--- scripts/usermodule_category.js
+++ scripts/usermodule_category.js
@@ -22,7 +22,7 @@ $.ajax({
           html += '<td>' + data.user_module_category_name + ' <code>' + data.module_count + ' modules</code></td>';
 
         }
-        html += '<td><a href="usermodule/list_view/' + data.user_module_category_id + '" class="pull-right btn btn-primary">view</a></td>';
+        html += '<td><a href="module-category/' + data.user_module_category_id + '" class="pull-right btn btn-primary">view</a></td>';
         html += '</tr>';
         $(table).append(html);
       });
