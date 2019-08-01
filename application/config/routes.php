bcc33e8cd965aeaabd9f21e3da619ca1a9a0b52a
Mark Daryl Jerezon <markdaryljerezon@gmail.com>
2019-08-01 17:01:21 +0800

    initial code for routes

--- application/config/routes.php
+++ application/config/routes.php
@@ -52,3 +52,57 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $route['default_controller'] = 'account';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
+
+// user
+$route['users'] = 'account/users';
+$route['user/(:num)'] = 'account/get_user_view/$1';
+$route['create/user'] = 'account/create_user_view';
+$route['update/user/(:num)'] = 'account/update_view/$1';
+
+// purchase order
+$route['purchase-orders'] = 'purchaseorder/purchase_order_view';
+$route['purchase-order/(:num)'] = 'purchaseorder/get_purchase_order_view/$1';
+$route['create/purchase-order'] = 'purchaseorder/create_purchase_order_view';
+$route['update/purchase-order/(:num)'] = 'purchaseorder/update_purchase_order_view/$1';
+
+// purchase order item
+$route['purchase-order-items/(:num)'] = 'purchaseorder/purchase_order_item_view/$1';
+$route['create/purchase-order-item/(:num)'] = 'purchaseorder/create_purchase_order_item_view/$1';
+$route['update/purchase-order-item/(:num)'] = 'purchaseorder/update_purchase_order_item_view/$1';
+
+// user module category
+$route['module-category'] = 'usermodulecategory/list_view';
+$route['create/module-category'] = 'usermodulecategory/create_view';
+$route['update/module-category/(:num)'] = 'usermodulecategory/update_view/$1';
+
+// user modules
+$route['module-category/(:num)'] = 'usermodule/list_view/$1';
+$route['create/module/(:num)'] = 'usermodule/create_view/$1';
+$route['update/module/(:num)'] = 'usermodule/update_view/$1';
+$route['assign/user-modules/(:num)'] = 'usermodule/assign_user_module_view/$1';
+
+// project
+$route['projects'] = 'project/list_view';
+$route['create/project'] = 'project/create_view';
+$route['update/project/(:num)'] = 'project/update_view/$1';
+
+// supplier
+$route['suppliers'] = 'supplier/list_view';
+$route['create/supplier'] = 'supplier/create_view';
+$route['update/supplier/(:num)'] = 'supplier/update_view/$1';
+
+// house
+$route['houses'] = 'house/list_view';
+$route['view/house'] = 'house/create_view';
+$route['create/house'] = 'house/create_view';
+$route['update/house/(:num)'] = 'house/update_view/$1';
+
+// warehouse
+$route['warehouses'] = 'warehouse/list_view';
+$route['view/warehouse/(:any)'] = 'stock/list_view/$1';
+$route['create/warehouse'] = 'warehouse/create_view';
+$route['update/warehouse/(:num)'] = 'warehouse/update_view/$1';
+
+// stock
+$route['create/stock-in'] = 'stock/create_stock_in_view';
+$route['create/stock-out'] = 'stock/create_stock_in_view';
