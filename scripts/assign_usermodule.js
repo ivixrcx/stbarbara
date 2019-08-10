3e61b9bb309da0b35e4384478da026466bb0ddfe
Mark Daryl Jerezon <markdaryljerezon@gmail.com>
2019-08-11 00:50:20 +0800

    get assigned modules

--- scripts/assign_usermodule.js
+++ scripts/assign_usermodule.js
@@ -1,14 +1,68 @@
+
 /* global declarations */
 
 var modules = [];
+var user_modules = '';
+var user_module_link = '';
+var table = $('table');
+var user_id = $('table').data('id');
 
 /**
  * 
  */
 
+// select modules assigned to current user
 $.ajax({
     type: 'post',
-    url: 'usermodules',
+    url: 'usermodule/get_assigned_user_modules',
+    data: { user_id: user_id },
+    error: function(err){
+        console.log('error');
+        console.log(err);
+    },
+    success: function(res){
+        if(res.has_data){
+            if(res.data.code == 101){
+                $('.block').html(res.data.error_html);
+            }
+
+            var user_modules = res.data;
+            
+            $.each(user_modules, function(key, user_module){
+
+                user_module_id = user_module.user_module_id;
+                td_modules = $(table).contents().find('td.modules');
+                
+                $.each(td_modules, function(key, td_module){
+
+                    td_user_module_id = $(td_module).data('id')
+                    if(td_user_module_id == user_module_id){
+                        
+                        // declare __user_module_id
+                        __user_module_id = "";
+
+                        module_link = $(td_module).contents().find('code.module-link').prevObject;
+                        $.each(module_link, function(key, link){
+
+                            if(__user_module_id != td_user_module_id){
+                                // update modules array
+                                modules.push(td_user_module_id);
+                            }
+                            // prevent repeated user_module_id in modules array
+                            __user_module_id = td_user_module_id;
+                        
+                            $(link).addClass('selected');
+                        })
+                       
+                    }
+                });
+
+            });
+        }
+        else{
+            // nothing
+        }
+    }
 });
 
 /**
