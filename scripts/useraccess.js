42af0213157c45dd51c746cba7adb75529dd2bae
Mark Daryl Jerezon <markdaryljerezon@gmail.com>
2019-08-27 11:46:45 +0800

    minor patch

--- scripts/useraccess.js
+++ scripts/useraccess.js
@@ -1,22 +1,52 @@
-$(function(){
-    $.ajax({
-        type: 'post',
-        url: 'account/get_session_permissions',
-        success: function(res){
-            let module_links = res.data;
-            let system_module = $('.system-module');
-            let system_modules = system_module.contents().find('a').prevObject;
-            
-            $.each( system_modules, function(key, module_html){
-                module_link = $(module_html).data('module');
-                
+
+
+/* global declarations */
+var __modules = $('.system-module a');
+var __request = $.post('account/get_session_permissions');
+
+// sidebar
+__request.done(function(result){
+    var module_links = result.data;
+    $.each( __modules, function(key, module_html){
+        module_link = $(module_html).data('module');
+
+        if($.inArray(module_link, module_links) == -1){
+            $(module_html).parent().remove();
+        }
+        else{
+            $(module_html).parent().removeAttr('style');
+        }
+    });
+});
+
+
+class useraccess {}
+// returns promise
+useraccess.prototype.request = function(){
+    return __request;
+}
+/* callable for re-initialization */
+// in-text
+useraccess.prototype.check = function(_modules){
+    __request.done(function(result){
+        var module_links = result.data;
+        $.each( _modules, function(key, module_html){
+            module_link = $(module_html).data('module');
+            $(module_html).click(function(e){
+                e.preventDefault();
                 if($.inArray(module_link, module_links) == -1){
-                    $(module_html).parent('.system-module').remove();
+                    Swal.fire({
+                        type: 'error',
+                        title: 'No Permission.',
+                        showConfirmButton: false,
+                        showCancelButton: false,
+                        timer: 1500
+                    });
                 }
                 else{
-                    $(module_html).parent('.system-module').removeAttr('style');
+                    window.location.href=e.target.href;
                 }
             });
-        }
-    })
-});
+        });
+    });
+}
