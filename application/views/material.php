533e698c11642c523d16341389115540f8dbe78d
Mark Daryl Jerezon <markdaryljerezon@gmail.com>
2019-09-09 21:01:45 +0800

    materials module initial code

---
+++ application/views/material.php
@@ -0,0 +1,29 @@
+<section class="no-padding-bottom">
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-lg-12">
+        <div class="block">
+          <div class="d-flex pull-right">
+            <a class="pr-2 pb-2 pt-2" href="create/material"><button class="btn btn-primary btn-sm">Add Material</button></a>
+          </div>
+          <div class="table-responsive pt-3">
+            <table class="table" style="width:100%"> 
+              <thead> 
+                <tr> 
+                  <th>particular</th>
+                  <th>category</th>
+                  <th>unit</th>
+                  <th>stocks</th>
+                  <th>last stock date</th>
+                  <th>stock level</th>
+                  <th></th>
+                </tr> 
+              </thead> 
+              <tbody id="list"></tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
---
+++ scripts/material.js
@@ -0,0 +1,41 @@
+
+/**
+ * 
+ */
+$('table').DataTable({
+  processing: true,
+  serverSide: true,
+  ajax: {
+    url: 'material/list',
+    type: 'post',
+    complete: function(res){
+        
+    },
+    error: function(error){
+      console.log('error');
+      console.log(error.responseText);
+    },
+  },
+  columns: [
+    {data: 'material_particular'},
+    {data: 'material_category_particular'},
+    {data: 'unit'},
+    {data: 'no_of_stocks'},
+    {data: 'last_stock_date'},
+    {data: 'stock_level'},
+    {data: null}
+  ],
+  columnDefs: [
+    { 
+      targets: 6,
+      render: function(data, type, row){
+          return `
+          <div class="d-flex justify-content-end">
+              <a href="view/material/${row['material_id']}" class="btn btn-primary btn-sm system-module" data-module="material/view"><i class="fa fa-eye text-dark"></i></a>
+              <a href="update/material/${row['material_id']}" class="pull-right btn btn-warning btn-sm ml-1 system-module" data-module="material/update_view"><i class="fa fa-pencil text-dark"></i></a>
+              <button class="pull-right btn btn-danger btn-sm ml-1 btndelete" data-material_id="${row['material_id']}"><i class="fa fa-remove text-dark"></i></button>
+          </div>`;
+      }
+    },
+  ]
+});
