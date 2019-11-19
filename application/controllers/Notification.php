db6d8aa6b2d1e83e65bff3d43cb912fe93001410
Mark Daryl Jerezon <markdaryljerezon@gmail.com>
2019-11-19 16:57:59 +0800

    initial notification controller and model

---
+++ application/controllers/Notification.php
@@ -0,0 +1,50 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Notification extends CI_Controller {
+
+	public function __construct()
+	{
+		parent::__construct();
+		$this->load->model( 'notification_model' );
+		$this->load->library( 'API', NULL, 'API' );
+		$this->load->library( 'UserAccess', array( $this ) );
+		$this->API->auth_required();
+		// $this->useraccess->check_permissions();
+	}
+
+	public function create()
+	{
+		$this->API->ajax_only();
+
+		$name 		 	 = $this->input->post( 'name' );
+		$lot_area  		 = $this->input->post( 'lot_area' );
+		$floor_area 	 = $this->input->post( 'floor_area' );
+		$suggested_price = $this->input->post( 'suggested_price' );
+
+		$create = $this->house_model->create();
+
+		if($create){
+			$this->API->emit_json( true );
+		}
+		else{
+			$this->API->emit_json( false, 'Error: insert');	
+		}
+    }
+    
+    public function flush() 
+    {
+        header('Content-Type: text/event-stream');
+        header('Cache-Control: no-cache');
+
+        $time = date('r');
+        echo "data: The server time is: {$time}\n\n";
+        flush();
+    }
+
+    public function test()
+    {
+        header('Content-Type: application/json');
+        print_r($this->notification_model->list(2));
+    }
+}
---
+++ application/models/Notification_model.php
@@ -0,0 +1,77 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Notification_model extends CI_Model {
+
+	function __construct()
+	{
+		parent::__construct();
+		$this->load->library('API');
+		$this->API = new API();
+    }
+    
+    public function list( $user_id, $limit=15 )
+    {
+        $this->db->select( 'user.full_name' );
+        $this->db->select( 'notification.user_id' );
+        $this->db->select( 'notification.scope' ); 
+        $this->db->select( 'notification.message' ); 
+        $this->db->select( 'notification.link' ); 
+        $this->db->select( 'notification.datetime' ); 
+        $this->db->select( 'notification.seen' ); 
+        $this->db->from( 'notification' );
+        $this->db->join( 'user', 'user_id', 'notification' );
+        $this->db->where( 'user_id', $user_id );
+        $this->db->order_by( 'notification.datetime', 'DESC' );
+        $this->db->limit( $limit );
+        return $this->db->get()->result();
+    }
+
+    public function unread( $user_id, $limit=15 )
+    {
+        $this->db->select( 'user.full_name' );
+        $this->db->select( 'notification.user_id' );
+        $this->db->select( 'notification.scope' ); 
+        $this->db->select( 'notification.message' ); 
+        $this->db->select( 'notification.link' ); 
+        $this->db->select( 'notification.datetime' ); 
+        $this->db->select( 'notification.seen' ); 
+        $this->db->from( 'notification' );
+        $this->db->join( 'user', 'user_id', 'notification' );
+        $this->db->where( 'user_id', $user_id );
+        $this->db->where( 'seen', false );
+        $this->db->order_by( 'notification.datetime', 'DESC' );
+        $this->db->limit( $limit );
+        return $this->db->get()->result();
+    }
+
+    public function mark_all_as_read( $user_id )
+    {
+        $this->db->set( 'seen', true );
+        $this->db->where( 'user_id', $user_id );
+        $this->db->update( 'notification' );
+        return $this->db->affected_rows();
+    }
+
+    public function create( $user_id, $user_type_id, $scope, $message, $link, $datetime, $seen )
+    {
+        $this->db->set( 'user_id', $user_id );
+        $this->db->set( 'user_type_id', $user_type_id );
+        $this->db->set( 'scope', $scope );
+        $this->db->set( 'message', $message );
+        $this->db->set( 'link', $link );
+        $this->db->set( 'datetime', $datetime );
+        $this->db->set( 'seen', $seen );
+        $this->db->insert( 'notification' );
+    }
+
+    public function update()
+    {
+        
+    }
+
+    public function delete()
+    {
+
+    }
+}
