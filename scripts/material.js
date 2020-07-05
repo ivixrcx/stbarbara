900d5a7ac0e4e4c8cf9333cf03f60f0699cf6174
Mark Daryl Jerezon <markdaryljerezon@gmail.com>
2020-07-06 01:00:04 +0800

    remove view in material

--- scripts/material.js
+++ scripts/material.js
@@ -36,7 +36,7 @@ $('table').DataTable({
       render: function(data, type, row){
           return `
           <div class="d-flex justify-content-end">
-              <a href="view/material/${row['material_id']}" class="btn btn-primary btn-sm system-module" data-module="material/view"><i class="fa fa-eye text-dark"></i></a>
+              <!-- <a href="view/material/${row['material_id']}" class="btn btn-primary btn-sm system-module" data-module="material/view"><i class="fa fa-eye text-dark"></i></a> -->
               <a href="update/material/${row['material_id']}" class="pull-right btn btn-warning btn-sm ml-1 system-module" data-module="material/update_view"><i class="fa fa-pencil text-dark"></i></a>
               <button class="pull-right btn btn-danger btn-sm ml-1 btndelete" data-material_id="${row['material_id']}"><i class="fa fa-remove text-dark"></i></button>
           </div>`;
