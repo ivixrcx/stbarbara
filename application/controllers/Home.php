ca194dfe1a45747b91c579b58d2c123de976a9a6
Mark Daryl Jerezon <markdaryljerezon@gmail.com>
2020-07-20 03:28:41 +0800

    add home title

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -14,6 +14,7 @@ class Home extends CI_Controller {
 	public function index()
 	{
 		$data = array();
+		$data['title'] = 'Home';
 		$data['nav_home'] = 'active';
 		$data['login_data'] = $this->session->userdata( 'login_data' );
 
